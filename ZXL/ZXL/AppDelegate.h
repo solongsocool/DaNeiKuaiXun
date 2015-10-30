1756f2c2b65346432c042797f8d7f56735476d67
solongsocool <ios-43@ACC8086A.ipt.aol.com>
2015-10-30 11:13:09 +0800

    version1
    1.1

---
+++ ZXL/ZXL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXL
+//
+//  Created by iOS－43 on 15/10/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
